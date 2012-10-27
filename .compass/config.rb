61295ac7a33e1c23151e378896ef1040734fcebd
Bryan Veloso <bryan@revyver.com>
2012-10-26 21:26:08 -0700

    Adding my convoluted static media setup.

---
+++ .compass/config.rb
@@ -0,0 +1,24 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/stylesheets"
+sass_dir = "assets/scss"
+images_dir = "static/images"
+javascripts_dir = "static/javascripts"
+
+output_style = :compressed
+# output_style = (environment == :production) ? :compressed : :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+asset_host = #<Proc:0x007f9d13b899f0@/Users/Bryan/Code/Revyver/hello-ranking/config.rb:20>
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+# line_comments = (environment == :production) ? false : true
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass ranking/static/scss scss && rm -rf sass && mv scss sass
